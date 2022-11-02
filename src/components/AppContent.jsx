97668cdca73fc97bb160c9c046cb103649d3f253
unknown <ugurcan.uut82@gmail.com>
2022-11-02 14:36:30 +0300

    update checkbox

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import TodoItem from './TodoItem'
+import styles from '../styles/modules/app.module.scss'
 
 export const AppContent = () => {
   const todoList = useSelector((state) => state.todo.todoList)
@@ -8,7 +9,7 @@ export const AppContent = () => {
   sortedTodoList.sort((a,b) => b.time.localeCompare(a.time))
   console.log(sortedTodoList)
   return (
-    <div>
+    <div className={styles.content__wrapper}>
       {sortedTodoList && sortedTodoList.length > 0 ? (
         sortedTodoList.map((todo) => <TodoItem key={todo.id} todo={todo}/>)
       ) : "Todo not found"}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 import { MdDelete, MdEdit } from 'react-icons/md'
 import { useDispatch } from 'react-redux'
-import { deleteTodo } from '../slices/todoSlice'
+import { deleteTodo, updateTodo } from '../slices/todoSlice'
 import styles from '../styles/modules/todoItem.module.scss'
 import { getClasses } from '../utils/getClasses'
+import CheckButton from './CheckButton'
 import { TodoModal } from './TodoModal'
 
 const TodoItem = ({todo}) => {
     const [updateModalOpen, setUpdateModalOpen] = useState(false)
-
+    const [checked, setChecked] = useState(true)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(todo.status === 'complete'){
+            setChecked(true)
+        } else {
+            setChecked(false)
+        }
+    },[todo.status])
 
     const handleDelete = () => {
         dispatch(deleteTodo(todo.id))
@@ -20,11 +30,21 @@ const TodoItem = ({todo}) => {
     const handleUpdate = () => {
         setUpdateModalOpen(true)
     }
+
+    const handleCheck = () => {
+        setChecked(!checked)
+        dispatch(updateTodo({
+            ...todo, 
+            status: checked ? 'incomplete' : 'complete' 
+        }))
+        toast.success("Updated Status")
+    }
+
   return (
     <>
         <div className={styles.item}>
             <div className={styles.todoDetails}>
-                []
+                <CheckButton checked={checked} handleCheck={handleCheck}></CheckButton>
                 <div className={styles.text}>
                     <p className={getClasses([styles.todoText, todo.status === 'complete' && styles['todoText--completed']])}>{todo.title}</p>
                     <p className={styles.time}>
